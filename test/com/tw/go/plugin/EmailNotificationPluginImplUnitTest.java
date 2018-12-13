014af55b09d63eaf0c9a28b1a61857d877cef7dc
Dan Worlton <worltondanielp@johndeere.com>
2018-12-13 13:17:04 -0600

    Update test dependencies and add regex for pipeline and stage filters

--- test/com/tw/go/plugin/EmailNotificationPluginImplUnitTest.java
+++ test/com/tw/go/plugin/EmailNotificationPluginImplUnitTest.java
@@ -24,8 +24,8 @@ import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
